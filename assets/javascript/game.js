8854af42d5afc175f89096a21f2e8b61620ff8b4
Kelly Mersereau <kellymersereau@Kelly-Mersereaus-MacBook-Pro.local>
2016-02-22 19:06:04 -0500

    this should have been done earlier

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,98 @@
-	var listOfAnimals = ["giraffe", "cat", "dog", "monkey"];
+window.onload = function () {
 
-	// 1. we choose a word that the uder has to guess from a list
+	var catergories; //array of topics
+	var chosenCategory; //selected category
+	var getHint; //word getHint
+	var word; //selected word
+	var guess; //guess
+	var guesses = []; //stored guesses
+	var lives; //lives
+	var counter; //count correct guesses
+	var space; //number of spaces in word '-'
 
-	var animal = listOfAnimals[Math.floor(Math.random() * listOfAnimals.length)];
+	//get elements
+	var showLives = document.getElementById("myLives");
+	var showCategory = document.getElementById("sCategory");
+	var getHint = document.getElementById("hint");
+	var showClue = document.getElementById("clue");
 
-	// 2. we build a view
+	//select category
+	var selectCat = function () {
+		if(chosenCategory === categories[0]) {
+			categoryName.innerHTML = "The category is Horror Movies";
+		} else if(chosenCategory === categories[1]) {
+			categoryName.innerHTML = "The category is Horror Villians";
+		} 
+	}
+//create guesses ul
+	result = function() {
+		wordHolder = document.getElementById('hold');
+		correct = document.getElementById('ul');
+
+		for (var i = 0; i < word.length; i++) {
+			correct.setAttribute('id', 'my-word');
+			guess = document.createElement('li');
+			guess.setAttribute('class', 'guess');
+		if(word[i] === "-") {
+			guess.innerHTML = "-";
+			space = 1;
+		}else {
+			guess.innerHTML = "-";
+		}
+
+		guesses.push(guess);
+		wordHolder.appendChild(correct);
+		correct.appendChild(guess);
+		}
+	}
+
+	//show lives
+	comments = function() {
+		showLives.innerHTML = "You have" + lives + "lives";
+		if(lives <1) {
+			showLives.innerHTML = "Game Over";
+		}
+		for(var i = 0; i < guesses.length; i++) {
+			if(counter + space === guesses.length){
+				showLives.innerHTML = "You win!";
+			}
+		}
+	}
+
+	//onClick function
+	check = function() {
+		list.onClick = function() {
+			var guess = (this.innerHTML);
+			this.setAttribute("class","active");
+			this.onClick = null;
+			for(var i = 0; i <word.length; i++) {
+				if(word[i].innerHTML = guess)
+			}
+		}
+	}
+	var horrorMovies = ["Scream", "Halloween", "Saw", "Nightmare on Elm Street", "The Last House on the Left", "I Know What You Did Last Summer", "Chuckie", "The Bride of Chuckie", "Alien", "The Purge", "Friday The 13th", "Zombeavers", "The Night of the Living Dead"];
+	var horror = horrorMovies[Math.floor(Math.random() * horrorMovies.length)];
+	var guessesLeft = 13;
+	var userGuesses =[];
 
 	var wordHTML ="";
 
-	for(var i=0; i < animal.length; i++){
-			if (animal[i] == 'a') {
+		for(var i=0; i < horror.length; i++){
+			if (horror[i] == 'a') {
 				wordHTML = wordHTML + "a";
 			}else {
 				wordHTML = wordHTML + " _ ";
-	}
+		}
 
 	document.querySelector('#game').innerHTML = wordHTML;
+
+	document.onkeyup = function(event) {
+		var userGuess = String.fromCharCode(event.keycode).toLowerCase();
+		userGuesses.push(userGuess);
+		doument.querySelector('#userGuesses').innerHTML = "User Guesses:" + userGuesses.join(', ');
+		
+
+
 
 
 	// 3. we let the user guess a letter from the keyboard
