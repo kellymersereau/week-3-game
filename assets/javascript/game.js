1d28b84e83d9e1b3cd1471044db03ce75edd6f29
Kelly Mersereau <kellymersereau@Kelly-Mersereaus-MacBook-Pro.local>
2016-02-27 18:07:21 -0500

    finally i figured it out

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,130 +1,119 @@
- 	// <div class="container">
- 	//  	<h2 id="categoryName"></h2>
- 	// 	<p id="myLives">Guesses Left: <br> 13 </p>
- 	// 	<button id="reset">Play again</button>
- 	// </div>
+// Global Variables (accessible by all functions))
+// =========================================================
 
- 	// <div id="game"></div>
-window.onload = function () {
-
-var listMovies = ["Scream", "Halloween", "Saw", "Nightmare on Elm Street", "The Last House on the Left", "I Know What You Did Last Summer", "Chuckie", "The Bride of Chuckie", "Alien", "The Purge", "Friday The 13th", "Zombeavers", "The Night of the Living Dead", "The Exorcist"];
+// Array of Word Options (all lowercase)
+var wordsList = ["scream", "halloween", "saw", "chuckie", "alien",  "zombeavers"];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var word ;//selected word to guess
-var guess; //user guesses
-var guesses; //stored guesses
-var lives ;//amount of lives
-var counter ;//count correct guesses
-var space ;// number of spaces in word
+var chosenWord = "";//selected word to guess - solution
+var lettersInChosenWord = []; //this will break the solution into individual letters to be stored in array
+var numBlanks = 0; //This will be the number of blanks we show based on the solution
+var blanksAndSuccesses = []; // Holds a mix of blank and solved letters (ex: 'n, _ _, n, _') 
+var wrongGuesses =[]; //holds all of the wrong guesses
 
-// elements in html
-var showLives = document.getElementById("myLives"); 
+//Game counters
+var winCounter = 0;
+var lossCounter = 0;
+var numGuesses = 13;
+
+//functions
+function startGame(){
+	numGuesses = 13;
+
+	chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)]; 
+	lettersInChosenWord = chosenWord.split(""); 
+	numBlanks = lettersInChosenWord.length; 
+
+	console.log(chosenWord);
+
+	blanksAndSuccesses = []; 
+	wrongGuesses = []; 
 
 
-
-
-//creates alphabet ul
-var buttons = function() {
-	myButtons = document.getElementById('buttons');
-	letters = document.createElement('ul');
-
-	for(var i=0; i < alphabet.length; i++){
-		letters.id ='alphabet';
-		list=document.createElement('li');
-		list.id='letter';
-		list.innerHTML=alphabet[i];
-		check();
-		myButtons.appendChild(letters);
-		letters.appendChild(list);
+	for(var i=0; i <numBlanks; i++){
+		blanksAndSuccesses.push("_");
 	}
+
+	console.log(blanksAndSuccesses); 
+	document.getElementById("guessesLeft").innerHTML = numGuesses;
+
+
+	document.getElementById('wordblanks').innerHTML = blanksAndSuccesses.join(" ");
+
+
+	document.getElementById('wrongGuesses').innerHTML = wrongGuesses.join(" ");
+
 }
 
 
+function checkLetters(letter) {
 
-//create guesses ul
-result = function() {
-	wordHolder=document.getElementById('hold');
-	correct = document.createElement('ul');
-
-	for(var i=0; i<word.length; i++){
-		correct.setAttribute('id', 'my-word');
-		guess = document.createElement('li');
-		guess.setAttribute('class', 'guess');
-
-		if(word[i] === "-") {
-		guess.innerHTML = "-";
-		space =1;
-		} else {
-		guess.innerHTML = "_";
-		}
-
-		guesses.push(guess);
-		word.Holder.appendChild(correct);
-		correct.appendChild(guess);
+	var letterInWord = false; 
+	for (var i=0; i<numBlanks; i++) {
+		if (chosenWord[i] == letter) {
+			letterInWord = true; 
+		};
 	}
-}
 
-//show lives
-comments=function() {
-	showLives.innerHTML = "You have " + lives + "guesses left.";
-	if(lives<1) {
-		showLives.innerHTML="Game Over";
-	}
-	for(var i=0; i<guesses.length; i++) {
-		if(counter + space === guesses.length) {
-			showLives.innerHTML = "You Win!";
-		}
-	}
-}
 
-// onclick function
-check = function () {
-	list.onClick = function () {
-		var guess = (this.innerHTML);
-		this.setAttribute("class", "active");
-		this.onClick = null;
-		for(var i = 0; i < word.length; i++){
-			if(word[i] === guess) {
-				guesses[i].innerHTML = guess;
-				counter += 1;
+	if (letterInWord) {
+
+		for(var i=0; i<numBlanks; i++){
+
+			if(chosenWord[i] == letter){
+				blanksAndSuccesses[i] = letter; 
 			}
 		}
-		var j = (word.indexOf(guess));
-			if(j === -1) {
-			lives -= 1;
-			comments();
-			} else {
-			comments();
-		}
+	console.log(blanksAndSuccesses); 
+
+	}
+
+	else {
+		wrongGuesses.push(letter); 
+		numGuesses--; 
 	}
 }
 
-//play
-play = function() {
-	word = listMovies[Math.floor(Math.random() * listMovies.length)];
-	word = word.replace(/\s/g, "-");
-	console.log(word);
-	buttons();
 
-	guesses = [];
-	lives = 13;
-	counter = 0;
-	space = 0;
-	comments();
+function roundComplete() {
 
-//build a view
-var wordHTML = "";
 
-for(var i=0; i < word.length; i++){
-	wordHTML = wordHTML + " _ ";
+	console.log("WinCount: " +winCounter + " | LossCount: " + lossCounter + " | NumGuesses: " + numGuesses);
+
+
+	document.getElementById("guessesLeft").innerHTML=numGuesses;
+	document.getElementById("wordblanks").innerHTML=blanksAndSuccesses.join(" "); 
+	document.getElementById("wrongGuesses").innerHTML=wrongGuesses.join(" ");
+
+
+	if(lettersInChosenWord.toString() == blanksAndSuccesses.toString()) {
+		winCounter++; 
+		alert("You win!"); 
+
+
+		document.getElementById("winCounter").innerHTML=winCounter;
+		startGame(); 
 	}
 
-document.querySelector('#hold').innerHTML=wordHTML;
+
+	else if(numGuesses == 0){
+		lossCounter++; 
+		alert("You lose :("); 
+
+	
+		document.getElementById("lossCounter").innerHTML=lossCounter;
+		startGame(); 
+	}
 }
-play();
 
 
+startGame();
 
 
+document.onkeyup = function(event){
+	letterGuessed = String.fromCharCode(event.keyCode).toLowerCase(); 
+	checkLetters(letterGuessed); 
+	roundComplete(); 
 }
+
